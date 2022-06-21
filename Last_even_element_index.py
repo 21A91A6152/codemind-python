0493f45fa3334bc18462b11adff0dbcd701f26fe
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-06-21 21:03:01 +0530

    n=int(input()) a=list(map(int,input().split())) x=0 for i in range(n-1,0,-1):     if(a[i]%2==0):         x=i         break if(x==0):     print('0') else:     print(x)

---
+++ Last_even_element_index.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+x=0
+for i in range(n-1,0,-1):
+    if(a[i]%2==0):
+        x=i
+        break
+if(x==0):
+    print('0')
+else:
+    print(x)
+       
