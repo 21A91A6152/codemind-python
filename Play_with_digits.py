2fa388c3e3a4bf19bedc497d23b58293d49840d0
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-06-30 22:40:29 +0530

    n=int(input()) a=list(map(int,input().split())) sum=0 for i in a:     while(i>0):         r=i%10         i=i//10         sum=sum+r print(sum)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,9 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
 sum=0
-for i in range(n):
-    while(a[i]):
-        j=a[i]%10
-        sum=sum+j
-        a[i]=a[i]//10
+for i in a:
+    while(i>0):
+        r=i%10
+        i=i//10
+        sum=sum+r
 print(sum)
