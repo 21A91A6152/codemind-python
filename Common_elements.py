0dabf84cd93ed67e09ed0a5cb41123f98a45be08
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-07-09 14:33:42 +0530

    x,y=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) c=[] d=[] for i in a:     if i in b:         c.append(i) for j in c:     if j not in d:         d.append(j) print(*d)

--- Common_elements.py
+++ Common_elements.py
@@ -1,13 +1,13 @@
-a,b=map(int,input().split())
-x=list(map(int,input().split()))
-y=list(map(int,input().split()))
-c=0
-b=[]
+x,y=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=[]
 d=[]
-for i in x:
-    if i in y:
-        b.append(i)
-for j in b:
+for i in a:
+    if i in b:
+        c.append(i)
+for j in c:
     if j not in d:
         d.append(j)
 print(*d)
+        
