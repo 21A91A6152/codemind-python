d30ae9f349f9eafcb2e6883de8618b8eb6647b25
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-07-09 11:12:32 +0530

    n=int(input()) a=list(map(int,input().split())) sum=0 for i in a:     sum+=i k=sum//n c=0 for i in a:     if i>=k:         c+=1 print(c)

--- elements_greater_than_average.py
+++ elements_greater_than_average.py
@@ -1,11 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
 sum=0
-c=0
-for i in range(n):
-    sum=sum+a[i]
+for i in a:
+    sum+=i
 k=sum//n
-for j in range(n):
-    if(a[j]>=k):
+c=0
+for i in a:
+    if i>=k:
         c+=1
 print(c)
