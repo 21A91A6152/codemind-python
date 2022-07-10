f2a1d77e7729400d88dc8eacf944fe2c395c88bc
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-07-10 11:41:56 +0530

    n=int(input()) a=list(map(int,input().split())) if(1 not in a):     print(1) elif(2 not in a):     print(2) elif(3 not in a):     print(3) elif(4 not in a):     print(4) elif(5 not in a):     print(5) else:     print(6)

---
+++ First_Missing_Positive.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+if(1 not in a):
+    print(1)
+elif(2 not in a):
+    print(2)
+elif(3 not in a):
+    print(3)
+elif(4 not in a):
+    print(4)
+elif(5 not in a):
+    print(5)
+else:
+    print(6)
