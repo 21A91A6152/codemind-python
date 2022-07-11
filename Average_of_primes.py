16f7720db6b535f385c8f904e42fa006c5cc0f6b
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-07-11 18:41:14 +0530

    def isprime(n):     for i in range(2,int(n**0.5)+1):         if(n%i==0):             return 0     else:         return 1 n=int(input()) a=list(map(int,input().split())) sum=c=0 for i in a:     if i==1:         continue     if(isprime(i)):         c+=1         sum=sum+i x=sum/c print("{:.2f}".format(x))

--- Average_of_primes.py
+++ Average_of_primes.py
@@ -1,19 +1,17 @@
 def isprime(n):
     for i in range(2,int(n**0.5)+1):
-        if n%i==0:
+        if(n%i==0):
             return 0
     else:
         return 1
 n=int(input())
 a=list(map(int,input().split()))
-c=0
-s=0
-for i in range(n):
-    if a[i]==1:
+sum=c=0
+for i in a:
+    if i==1:
         continue
-    if isprime(a[i]):
+    if(isprime(i)):
         c+=1
-        s=s+a[i]
-k=s/c        
-print('{:.2f}'.format(k))        
-        
+        sum=sum+i
+x=sum/c
+print("{:.2f}".format(x))
