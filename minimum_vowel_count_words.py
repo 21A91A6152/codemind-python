6cdc51b0a4df78bc2b59c2974a16b72a1858f424
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-07-12 10:10:54 +0530

    n=input() b=[] c=0 for i in n:     if(i=='a' or i=='e' or i=='i' or i=='o' or i=="u"):         c+=1     if(i==' '):         b.append(c)         c=0 b.append(c) x=min(b) d=0 for i in b:     if(i==x):         d+=1 print(d)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,16 @@
+n=input()
+b=[]
+c=0
+for i in n:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=="u"):
+        c+=1
+    if(i==' '):
+        b.append(c)
+        c=0
+b.append(c)
+x=min(b)
+d=0
+for i in b:
+    if(i==x):
+        d+=1
+print(d)
