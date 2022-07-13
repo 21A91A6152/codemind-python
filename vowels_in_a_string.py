9fab28046f151659faeffe779afa78f2ff08b01c
21A91A6152 <97424483+21A91A6152@users.noreply.github.com>
2022-07-13 23:54:00 +0530

    n=input() x=input() for i in n:     if(i==x):         print(True)         print(n.index(i))         break else:     print(False)

--- vowels_in_a_string.py
+++ vowels_in_a_string.py
@@ -1,7 +1,9 @@
-s=input()
-k=input()
-if k in s:
-    print(True)
-    print(s.find(k))
+n=input()
+x=input()
+for i in n:
+    if(i==x):
+        print(True)
+        print(n.index(i))
+        break
 else:
     print(False)
