b5e0732c2fc730dc8842a2aae50705cdf25501db
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2022-10-29 11:27:16 +0530

    n=input() x=list(n.split(" ")) print(min(x[0]),max(x[len(x)-1]),end=" ")

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,12 +1,5 @@
 n=input()
-b=[]
-x=''
-y=''
-for i in n.split(" "):
-    x=i
-    b.append(min(x))
-    break
-for i in n.split(" "):
-    y=i
-b.append(max(y))
-print(*b)
+x=list(n.split(" "))
+print(min(x[0]),max(x[len(x)-1]),end=" ")
+    
+    
