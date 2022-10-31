af450f68d1dafb2055df5e140cf1571d73be20c6
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2022-10-31 13:54:13 +0530

    a=input() b=input() a=a.lower() b=b.lower() x=list(a.split(" ")) y=list(b.split(" ")) c=0 for i in x:     if i in y:         if(x.count(i)==1 and y.count(i)==1):             c+=1 print(c)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -1,11 +1,12 @@
-n=input()
-m=input()
-a=list(n.split(" "))
-b=list(m.split(" "))
+a=input()
+b=input()
+a=a.lower()
+b=b.lower()
+x=list(a.split(" "))
+y=list(b.split(" "))
 c=0
-for i in a:
-    if i in b:
-        if(a.count(i)==1):
-            if(b.count(i)==1):
-                c+=1
+for i in x:
+    if i in y:
+        if(x.count(i)==1 and y.count(i)==1):
+            c+=1
 print(c)
